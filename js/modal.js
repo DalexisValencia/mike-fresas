3ce33317993a78808be64204f70a9a6b6086fb69
DalexisValencia <d.alexis.valencia@gmail.com>
2021-03-26 13:40:04 -0500

    añadiendo los botones de modales de prueba

--- js/modal.js
+++ js/modal.js
@@ -2,15 +2,19 @@ document.addEventListener("DOMContentLoaded", function() {
     var modal = document.getElementById("modal");
     var openModal = document.getElementById("exampleBtn");
     var closeModal = document.getElementById("closeModal");
-    var modalType = openModal.getAttribute('data-modal')
-    openModal.onclick = function() {
-        modal.style.display = "block";
-        for (let el of document.querySelectorAll('.modal-body > *')) el.style.display = 'none';
-        for (let el of document.querySelectorAll('.' + modalType)) el.style.display = 'block';
-        if (modalType == 'push1') {
-            document.querySelectorAll('.shared')[0].style.display = 'flex'
+    var openModalClass = document.getElementsByClassName('openModal');
+    for (let el of openModalClass) {
+        el.onclick = function() {
+            var modalType = el.getAttribute('data-modal');
+            modal.style.display = "block";
+            for (let el of document.querySelectorAll('.modal-body > *')) el.style.display = 'none';
+            for (let el of document.querySelectorAll('.' + modalType)) el.style.display = 'block';
+            if (modalType == 'push1') {
+                document.querySelectorAll('.shared')[0].style.display = 'flex'
+            }
         }
     }
+
     closeModal.onclick = function() {
         modal.style.display = "none";
     }
